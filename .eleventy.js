86e62340a3b8c6b877956f4807f6b19928299a49
Amy Kapernick <amy@kapers.dev>
2021-03-16 08:46:03 +0800

    adding netlify cms

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,10 @@
 module.exports = function (eleventyConfig) {
-  eleventyConfig.addPassthroughCopy("logo.png");
-  eleventyConfig.addPassthroughCopy("favicon.ico");
-  eleventyConfig.addPassthroughCopy("safari-pinned-tab.svg");
-  eleventyConfig.addPassthroughCopy("robots.txt");
+	eleventyConfig.setQuietMode(true);
+
+	return {
+		dir: {
+			input: "site",
+			markdownTemplateEngine: 'njk'
+		},
+	}
 };
