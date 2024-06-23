6ed540dec962ee4fa381d988c1d2ef67415e63b8
Amy Kapernick <amy@kapers.dev>
2024-06-23 09:39:13 +0800

    changed to svg and cleaned some up

--- .github/actions/qrcode.sh
+++ .github/actions/qrcode.sh
@@ -6,14 +6,14 @@ lookup_md="# Redirects\n\n"
 for redirect in $(echo "${redirects}" | jq -r '.[] | @base64'); do
 	slug=$(echo ${redirect} | base64 --decode | jq -r '.slug')
 	url=$(echo ${redirect} | base64 --decode | jq -r '.url')
-	lookup_md="${lookup_md}<details>\n<summary><strong>${slug}</strong> → ${url}</summary>\n\n[${domain}/${slug}](${domain}/${slug})\n\n![QR code for ${slug}](./site/src/img/qr_codes/${slug}.png)\n\n</details>\n"
+	lookup_md="${lookup_md}<details>\n<summary><strong>${slug}</strong> → ${url}</summary>\n\n[${domain}/${slug}](${domain}/${slug})\n\n![QR code for ${slug}](./site/src/img/qr_codes/${slug}.svg)\n\n</details>\n"
 
-	if [ -f "site/src/img/qr_codes/${slug}.png" ]; then
+	if [ -f "site/src/img/qr_codes/${slug}.svg" ]; then
 		echo "QR code for ${slug} already exists"
 		continue
 	fi
 
-	npx qrcode "${domain}/${slug}" -o "site/src/img/qr_codes/${slug}.png"
+	npx qrcode "${domain}/${slug}" -t svg -q 1 -o "site/src/img/qr_codes/${slug}.svg"
 done
 
 echo -e $lookup_md > lookup.md
